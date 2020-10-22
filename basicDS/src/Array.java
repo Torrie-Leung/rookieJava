3136c385d60e415dadb4e5f3facef292c78eb451
TorrieL <hmminicity2@163.com>
2020-10-22 20:39:05 +0800

    add num into anywhere

--- basicDS/src/Array.java
+++ basicDS/src/Array.java
@@ -26,10 +26,29 @@ public class Array {
     }
     //向最后添加元素
     public void addLast(int e){
+        // if(size == data.length)
+        //     throw new IllegalArgumentException("AddLast failed. Array is full");
+        // data[size] = e;
+        // size++;
+
+        add(size, e);
+
+    }
+     //向所有元素前添加1个新元素
+     public void addFirst(int e){
+        add(0,e);
+
+    }
+    //在第index个位置插入一个新元素e
+    public void add(int index, int e){
         if(size == data.length)
             throw new IllegalArgumentException("AddLast failed. Array is full");
-        data[size] = e;
-        size++;
+        if(index < 0 || index > size)
+            throw new IllegalArgumentException("AddLast failed.Require index >=0 Array is full");
+        for(int i = size - 1; i >= index; i--)
+        data[i + 1] = data[i];
 
+        data[index] = e;
+        size ++;
     }
 }
