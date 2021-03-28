862997058624076eb52f8ce676bebcf03f2e0fa5
TorrieL <hmminicity2@163.com>
2021-03-28 12:56:32 +0800

    long time no java basic

---
+++ basicDS/src/ex01/ex01.java
@@ -0,0 +1,12 @@
+package ex01;
+
+public class ex01 {
+    public static void main(String[] args){
+        /*
+        * hahahah multiple lines
+        * */
+        System.out.println("idiot!");
+        int n = 3;
+        System.out.println(n);
+    }
+}
